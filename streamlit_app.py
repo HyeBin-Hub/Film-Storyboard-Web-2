92f10775c3e69ed6601cf1c25a6866fea79330cc
HyeBin-Hub <gpqls00415@gmail.com>
2026-07-06 19:42:07 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import io
 import pandas as pd
 import streamlit as st
 
-from backend import run_csv_parser_test
+from backend import run_csv_parser_test, run_face_generation
 
 # =========================
 # Fixed Values
